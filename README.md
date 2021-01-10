8847ae9c23e18c5d08bcdef3cac6af41aae77097
Rusty Conover <rusty@conover.me>
2021-01-10 00:10:43 -0500

    Change to five minute updates, add range, volume and change rather than actual price.

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Import Finviz.com Futures to Microprediction
 
-This module imports various futures prices to Microprediction.org.
+This module imports various futures prices to [Microprediction.org](https://microprediction.org).
 
 ## Implementation Details
 
 There is a single Lambda function that is run as a scheduled
-CloudWatch Event every minute pull new data. This function
-is created using webpack to amalgamate the various imported modules.
-
-It runs in about 2 seconds or less every minute.
-
-The write keys are not included in this repo.
+CloudWatch Event every minutes to pull new data. This function
+is created using Webpack to amalgamate the various imported modules.
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,147 @@
 // What this code does is download the latest load information and publishes to microprediction.org
 import { MicroWriter, MicroWriterConfig } from "microprediction";
-import { stream_write_keys } from "./write-keys";
 const bent = require("bent");
 
-import * as _ from "lodash";
+import * as AWS from "aws-sdk";
+import * as qs from "querystring";
 const getJSON = bent("json");
 const getBuffer = bent("buffer");
 import { ScheduledHandler } from "aws-lambda";
-import moment from "moment-timezone";
-import * as cheerio from "cheerio";
 
-async function getFutures(): Promise<
-  Array<{
-    name: string,
-    value: number
-  }>
-> {
-  // The file is updated every five minutes.
-  const html = await getBuffer("https://finviz.com/futures.ashx");
-  const $ = cheerio.load(html.toString('utf8'));
+type FutureInfo = {
+  name: string,
+  last_update_time: number,
+  last_volume: number,
+  last_range: number,
+  last_change: number,
+};
 
-  const script_tags = $('body > script')
-    .map((i, x) => x.children[0]);
+async function getFuture(ticker: string): Promise<FutureInfo> {
+  const quote_options = {
+    ticker,
+    instrument: "futures",
+    timeframe: "i5",
+    type: "new"
+  };
 
-  const script_data = script_tags.get(1).data;
-  const script_lines: string[] = script_data.split(/\n/).filter((v: string) => v.match(/(var groups|var tiles)/));
+  const json = await getJSON(`https://finviz.com/api/quote.ashx?${qs.encode(quote_options)}`);
 
-  //console.log(script_lines);
+  //  console.log(json);
+  return {
+    last_update_time: json.date[json.date.length - 1],
+    last_volume: json.volume[json.volume.length - 1],
+    last_range: json.lastHigh - json.lastLow,
+    last_change: json.close[json.close.length - 1] - json.close[json.close.length - 2],
+    name: json.name,
+  }
+}
 
-  //const group_line = JSON.parse(script_lines.filter(v => v.match(/var groups = (.*)/))[0].replace(/var groups = /, '').replace(/;$/, ''));
 
-  const tiles_line = JSON.parse(script_lines.filter(v => v.match(/var tiles = (.*)/))[0].replace(/var tiles = /, '').replace(/;$/, ''));
 
-  //console.log(group_line);
-  //console.log(tiles_line);
+async function getFutures(): Promise<FutureInfo[]> {
 
-  const result = Object.values(tiles_line).map((t: any) => {
-    return {
-      name: t.label,
-      value: t.last
-    };
-  });
 
-  //  console.log(result);
-  return result;
+  const futures_to_pull = [
+    "YM",
+    "NQ",
+    "ES",
+    "ER2",
+    "NKD",
+    "EX",
+    "DY",
+    "VX",
+    "CL",
+    "QA",
+    "RB",
+    "HO",
+    "NG",
+    "ZK",
+    "ZB",
+    "ZN",
+    "ZF",
+    "ZT",
+    "CC",
+    "CT",
+    "JO",
+    "KC",
+    "LB",
+    "SB",
+    "GC",
+    "SI",
+    "PL",
+    "HG",
+    "PA",
+    "LC",
+    "FC",
+    "LH",
+    "ZC",
+    "ZL",
+    "ZM",
+    "ZO",
+    "ZR",
+    "ZS",
+    "ZW",
+    "RS",
+    "DX",
+    "6E",
+    "6J",
+    "6B",
+    "6C",
+    "6S",
+    "6A",
+    "6N",
+  ];
+
+  const results = await Promise.all(futures_to_pull.map(getFuture));
+  return results;
+}
+
+interface FinvizState {
+  [name: string]: number
+}
+const s3 = new AWS.S3({ region: "us-east-1" });
+
+async function getLastUpdate(): Promise<FinvizState | undefined> {
+  try {
+    const state = await s3.getObject({
+      Bucket: 'microprediction-lambda',
+      Key: 'finviz-state.json',
+    }).promise();
+    if (state.Body != null) {
+      return JSON.parse(state.Body?.toString('utf8'));
+    }
+  } catch (e) {
+    console.error(`Error retrieving state: ${e}`);
+    return;
+  }
+}
+
+async function setState(s: FinvizState) {
+  await s3.putObject({
+    Bucket: 'microprediction-lambda',
+    Key: 'finviz-state.json',
+    Body: JSON.stringify(s),
+  }).promise();
 }
 
 
 async function pushFutures() {
+  const last_update = await getLastUpdate();
   const futures = await getFutures();
+
+  let futures_to_send = [];
+  if (last_update == null) {
+    futures_to_send = futures;
+  } else {
+    futures_to_send = futures.filter(record => {
+      // @ts-ignore
+      return last_update != null && (last_update[record.name] == null || last_update[record.name] !== record.last_update_time);
+    });
+  }
+
+  console.log("Would send", futures_to_send.length, "records");
+
+  await setState(Object.fromEntries(futures.map(v => [v.name, v.last_update_time])));
 
   const writes = [];
 
@@ -61,12 +154,16 @@ async function pushFutures() {
   });
   const writer = new MicroWriter(config);
 
-  for (const { name, value } of futures) {
-    const clean_name = name.toLowerCase().replace(/ /g, "_")
+  for (const record of futures_to_send) {
+    const clean_name = record.name.toLowerCase().replace(/ /g, "_")
       .replace(/&/g, '_')
       .replace(/,/g, "");
-    console.log("Writing", clean_name, value);
-    writes.push(writer.set(`finviz-futures-${clean_name}.json`, value));
+    console.log("Writing", clean_name);
+
+    console.log(`${clean_name} change: ${record.last_change} range: ${record.last_range} volume: ${record.last_volume}`);
+    writes.push(writer.set(`finance-futures-${clean_name}-change.json`, record.last_change));
+    writes.push(writer.set(`finance-futures-${clean_name}-range.json`, record.last_range));
+    writes.push(writer.set(`finance-futures-${clean_name}-volume.json`, record.last_volume));
   }
   await Promise.all(writes);
 }
